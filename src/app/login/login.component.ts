926972bf913bbdcf14579ec6760e91209e0b25e4
Matt Rangel <mattrangel@gmail.com>
2022-07-07 16:34:19 -0700

    added router to constructor ability to navigate

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,11 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor() { }
+  //Router
+  //Angular.giveMeRouter
+  //Dependency Injection
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +25,8 @@ export class LoginComponent implements OnInit {
   handleLogin(){
     // console.log(this.username)
     if(this.username==="admin" && this.password==="admin"){
+      //Redirect to Welcome Page
+      this.router.navigate(['welcome', this.username])
       this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
