d8f705318a301593c4f96733271807a3be1ae466
Matt Rangel <mattrangel@gmail.com>
2022-07-11 16:12:47 -0700

    Developed Eclipse backend and connected

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,6 +3,7 @@
 // import org.springframework.boot.SpringApplication;
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 // import { AppComponent } from '../app.component';
 
 // @ComponentScan(
@@ -18,25 +19,51 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   message = 'Welcome again!'
+  welcomeMessageFromService:string | undefined
+
   name = ''
 
   // public SpringBootFirstWebApplication(){
 
   //ActivatedRoute
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService) { }
 
-  ngOnInit(){
-    console.log(this.message)
+  ngOnInit() {
+    // console.log(this.message)
     // console.log(this.route.snapshot.params['name'])
     this.name = this.route.snapshot.params['name']
   }
 
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response)
+      error => this.handleErrorResponse(error)
+    );
+    console.log('last line of getWelcomeMessage')
+    // console.log("get welcome message");
+  }
+
+  handleSuccessfulResponse(response: HelloWorldBean){
+    this.welcomeMessageFromService = response.message
+    // console.log(response);
+    // console.log(response.message);
+  }
+  handleErrorResponse(error) {
+    this.welcomeMessageFromService =
+  }
 }
 
-  export class Class1 {
 
-  }
 
-  export class Class2 {
 
-  }
+export class Class1 {
+
+}
+
+export class Class2 {
+
+}
