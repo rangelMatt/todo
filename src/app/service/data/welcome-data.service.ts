08817508a27a877322f40b2322c081bc97311813
Matt Rangel <mattrangel@gmail.com>
2022-07-11 20:57:03 -0700

    new method to pass param from welcome component

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export class HelloWorldBean {
@@ -9,13 +9,23 @@ export class HelloWorldBean {
   providedIn: 'root'
 })
 export class WelcomeDataService {
+  // executeHelloWorldServiceWithPathVariable(name: string) {
+  //   // throw new Error('Method not implemented.');
+  // }
 
   constructor(
-    private http:HttpClient,
+    private http: HttpClient,
   ) { }
 
-  executeHelloWorldBeanService(){
-  return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean')
+  executeHelloWorldBeanService() {
+    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean')
     // console.log("Execute Hello World Bean Service");
+  }
+  //http://localhost:8080/hello-world/path-variable/centralcoastbarbell
+  executeHelloWorldServiceWithPathVariable(name: string) {
+    return this.http.get<HelloWorldBean>(
+      `http://localhost:8080/hello-world/path-variable/${name}`,
+    )
+
   }
 }
