887e03aa0c05d547d6008db7ed7b2297c2a921f0
Matt Rangel <mattrangel@gmail.com>
2022-07-12 13:45:20 -0700

    Created a route to backend to see live updates

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -19,4 +19,8 @@ export class TodoDataService {
   deleteTodo(username: any, id: any) {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`)
   }
+
+  retrieveTodo(username: any, id: any) {
+    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`)
+  }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Todo } from '../list-todos/list-todos.component';
+import { TodoDataService } from './../service/data/todo-data.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  id: number | undefined
+  todo!: Todo;
+
+  constructor(
+    private todoService: TodoDataService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.todo = new Todo(1, '',false,new Date());
+    this.todoService.retrieveTodo('centralcoastbarbell', this.id)
+    .subscribe(
+      data => this.todo = data
+    )
   }
 
 }
